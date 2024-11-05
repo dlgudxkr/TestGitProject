30157ea2bb7580f27b7cf2bbd0cc4cecc2d1e40a
yunjia <yunjia@DESKTOP-J1RHQ56.mshome.net>
2024-11-05 11:58:21 +0900

    TEST

--- main/testMain.java
+++ main/testMain.java
@@ -1,10 +1,7 @@
-
-
-
 public class testMain {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Hello, worl d!");
+        System.out.println("Hello, worl d2!");  
         System.out.println("Hello, wo rld!");
         System.out.println("Hello, w orld!");
     }
