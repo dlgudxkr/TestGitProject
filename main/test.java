78703f0c37aa67961e7e897a288cb0b64cc57cb1
yunjia <yunjia@DESKTOP-J1RHQ56.mshome.net>
2024-11-05 11:53:15 +0900

    test

--- main/test.java
+++ main/test.java
@@ -2,8 +2,8 @@
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, worl d!");
+        System.out.println("Hello, worl d!");
         System.out.println("Hello, world!");        System.out.println("Hello, world!");
 
         System.out.println("Hello, world!");        System.out.println("Hello, world!");
@@ -12,6 +12,22 @@ public class test {
 
 
 asdfdsafdsfds
+        //testsettestestset
+    }
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, worl d!");
+        System.out.println("Hello, worl d!");
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+
+
         //testsettestestset
     }
 }
