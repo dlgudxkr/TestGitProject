a40e5e045809b582a3aefe1d2cf51a1f62b61e12
tak <dlgudxkr@icert.co.kr>
2024-11-05 11:35:36 +0900

    수정

--- main/test.java
+++ main/test.java
@@ -1,1 +1,10 @@
-tesestestestestset
+
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
+    }
+}
