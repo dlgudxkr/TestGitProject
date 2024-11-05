2fe97883e59a99e62acac0ea06c50cc6ff9bdac8
tak <dlgudxkr@icert.co.kr>
2024-11-05 11:47:39 +0900

    수정

--- main/testMain.java
+++ main/testMain.java
@@ -3,8 +3,8 @@
 public class testMain {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, worl d!");
+        System.out.println("Hello, wo rld!");
         System.out.println("Hello, w orld!");
     }
 }
