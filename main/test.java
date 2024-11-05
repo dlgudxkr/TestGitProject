6edca2fd9cecdafd8e9062b575905630888d5410
tak <dlgudxkr@icert.co.kr>
2024-11-05 11:42:24 +0900

    수정

--- main/test.java
+++ main/test.java
@@ -4,8 +4,13 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+        System.out.println("Hello, world!");        System.out.println("Hello, world!");
+
+
 
         //testsettestestset
     }
--- main/testMain.java
+++ main/testMain.java
@@ -5,6 +5,6 @@ public class testMain {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!");
         System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
+        System.out.println("Hello, w orld!");
     }
 }
